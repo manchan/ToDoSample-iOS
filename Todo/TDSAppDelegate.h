583f947b3312d60ecaf05f106424245543de3560
yuichi matsuoka <anything12new14@gmail.com>
2014-06-21 22:35:11 +0900

    Initial Commit

---
+++ Todo/TDSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDSAppDelegate.h
+//  Todo
+//
+//  Created by matz on 2014/06/21.
+//  Copyright (c) 2014年 matz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Todo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Todo
+//
+//  Created by matz on 2014/06/21.
+//  Copyright (c) 2014年 matz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDSAppDelegate class]));
+    }
+}
